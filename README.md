0668e10128c4431a3d689fca9c6d60adcf3a1dbc
Lars Thorup <lars@zealake.com>
2020-10-17 10:30:01 +0200

    test google play store

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # appium-sandbox
 
+Prerequisites
+
+http://appium.io/docs/en/drivers/android-uiautomator2/#real-device-setup
+
 ```
 npm install
 npm run doctor
+```
+
+Terminal 1
+
+```
+npm run appium
+```
+
+Terminal 2
+
+```
 adb devices
 node index (deviceId)
 ```
--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const wdio = require("webdriverio");
-const assert = require("assert");
+const wdio = require('webdriverio');
+const assert = require('assert');
+const fs = require('fs');
 
 const deviceName = process.argv[2];
 
 const opts = {
-  path: "/wd/hub",
+  path: '/wd/hub',
   port: 4723,
   capabilities: {
-    platformName: "Android",
-    platformVersion: "10",
+    platformName: 'Android',
+    platformVersion: '10',
     deviceName,
-    app: "ApiDemos-debug.apk", // From http://appium.io/docs/en/about-appium/getting-started/
-    appPackage: "io.appium.android.apis",
-    appActivity: ".view.TextFields",
-    automationName: "UiAutomator2",
+    // app: "ApiDemos-debug.apk", // From http://appium.io/docs/en/about-appium/getting-started/
+    // appPackage: "io.appium.android.apis",
+    appPackage: 'com.android.vending', // Goole Play Store
+    appActivity: 'com.google.android.finsky.activities.MainActivity',
+    automationName: 'UiAutomator2',
   },
 };
 
 async function main() {
   const client = await wdio.remote(opts);
 
-  const field = await client.$("android.widget.EditText");
-  await field.setValue("Hello World!");
-  assert.equal(await field.getText(), "Hello World!");
+  // const field = await client.$("android.widget.EditText");
+  // await field.setValue("Hello World!");
+  // assert.equal(await field.getText(), "Hello World!");
 
-  await field.setValue("Hello Lars!");
-  assert.equal(await field.getText(), "Hello Lars!");
+  // await field.setValue("Hello Lars!");
+  // assert.equal(await field.getText(), "Hello Lars!");
 
+  client.setImplicitTimeout(10000);
+  const searchField = await client.findElement(
+    'xpath',
+    "//android.widget.TextView[@text='Search for apps & games']"
+  );
+  client.setImplicitTimeout(1000);
+
+  const screenshot = await client.takeScreenshot();
+  fs.writeFileSync('screenshot.png', new Buffer(screenshot, 'base64'));
+  const pageSource = await client.getPageSource();
+  fs.writeFileSync('pagesource.xml', pageSource);
   await client.deleteSession();
 }
 
